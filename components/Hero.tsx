f3a36fa7276b4022781cfd929d79dc995bec89c2
dmitrii <shkarebnii03@gmail.com>
2024-09-04 18:49:37 +0300

    Оформление начальной страницы и внесение своих изменений в код

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,39 @@
+"use client"
+
 import Image from 'next/image';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import CustomButton from './CustomButton';
 
-export default Hero
+const Hero = () => {
+    const handleScroll = () => {
+
+    }
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+             Найти, забронировать или арендовать автомобиль – быстро и легко!   
+            </h1> 
+
+            <p className="hero__subtitle">
+            Улучши свой опыт аренды автомобиля с помощью нашего сервиса.     
+            </p>   
+     
+           <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
