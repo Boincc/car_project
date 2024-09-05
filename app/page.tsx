cb2b38a69644b5bdecab843ce0afb81307ba9887
dmitrii <shkarebnii03@gmail.com>
2024-09-05 14:54:29 +0300

    Поисковая строка авто по маркам

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 
 export default function Home() {
   return (
     <main 
     className="overlow-hidden">
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home_filters">
+          <SearchBar/>
+
+          <div
+          className="home__filter-container">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
